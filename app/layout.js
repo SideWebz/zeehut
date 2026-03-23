d2ca62655676fcb7538d0bdf60a3d10a6af2ebd4
SideWebz <webzside@gmail.com>
2026-03-23 23:29:15 +0100

    tracking

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Script from "next/script"; // <-- voeg dit toe
 
 export const metadata = {
   title: {
@@ -16,11 +17,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="nl" data-scroll-behavior="smooth">
       <body>
+        {/* Umami Tracking Script */}
+        <Script
+          strategy="afterInteractive"
+          src="https://analytics.sidewebz.be/script.js"
+          data-website-id="6cd043e8-e75c-412d-8b33-be8d517ec4d6"
+        />
+
         <Navbar />
 
-        <main className="container mt-4">
-          {children}
-        </main>
+        <main className="container mt-4">{children}</main>
 
         <Footer />
       </body>
